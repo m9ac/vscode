4023235908ec42bcaf2ab8534f8dcc50e8ba266e
Micah Cerasani <Micah@Cerasani.net>
2023-06-28 08:21:50 +0000

    done

--- pythonTest/Data models/netconfHelloWorld.py
+++ pythonTest/Data models/netconfHelloWorld.py
@@ -1,10 +1,10 @@
 from ncclient import manager
 
 router = {
-    "host" : "ios-xe-mgmt-latest.cisco.com",
-    "port" : "10000",
-    "username" : "developer",
-    "password" : "C1sco12345",
+    "host":"ios-xe-mgmt-latest.cisco.com",
+    "port":"10000",
+    "username":"developer",
+    "password": "C1sco12345",
 }
 
 with manager.connect(**router, hostkey_verify=False) as m:
