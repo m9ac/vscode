7433bcf778fc321b46de85b6bcfcb09151667b98
Micah Cerasani <Micah@Cerasani.net>
2023-07-01 09:07:47 +0000

    updated for naming consistancy

---
+++ ENAUTO_practice/Netconf/learningnetconfPOLISHED/netconfEditInterfaces.py
@@ -0,0 +1,14 @@
+from ncclient import manager
+from router_info import router
+
+with open('/home/micah/vscode/learningnetconf/ios_config.xml') as file:
+    config_template = file.read()
+
+
+netconf_config = config_template.format(
+    interface_name="GigabitEthernet2", interface_desc="Micah w here")
+
+with manager.connect(**router, hostkey_verify=False) as m:
+    response = m.edit_config(netconf_config, target="candidate")
+
+print(response)
