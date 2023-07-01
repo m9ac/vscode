07cfe951f1c2ef757a04c23be0d3b69448f6d74a
Micah Cerasani <Micah@Cerasani.net>
2023-07-01 09:11:05 +0000

    spellchecked

--- ENAUTO_practice/Netconf/learningnetconfPOLISHED/netconfEditInterfaces.py
+++ ENAUTO_practice/Netconf/learningnetconfPOLISHED/netconfEditInterfaces.py
@@ -6,7 +6,7 @@ with open('/home/micah/vscode/learningnetconf/ios_config.xml') as file:
 
 
 netconf_config = config_template.format(
-    interface_name="GigabitEthernet2", interface_desc="Micah w here")
+    interface_name="GigabitEthernet2", interface_desc="Micah was here")
 
 with manager.connect(**router, hostkey_verify=False) as m:
     response = m.edit_config(netconf_config, target="candidate")
