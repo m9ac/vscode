ec05e39e78d279e379d126b4e601e866d1d44a6c
Micah Cerasani <Micah@Cerasani.net>
2023-07-01 01:44:53 +0000

    not working

--- learningnetconf/ENAUTO_EDIT.py
+++ learningnetconf/ENAUTO_EDIT.py
@@ -1,12 +1,14 @@
 from ncclient import manager
 from router_info import router
 
-config_template = open("/home/micah/vscode/learningnetconf/ios_config.xml").read()
+with open('/home/micah/vscode/learningnetconf/ios_config.xml') as file:
+    config_template = file.read()
+
 
 netconf_config = config_template.format(
-    interface_name="GigabitEthernet2", interface_desc="Micah was here")
+    interface_name="GigabitEthernet2", interface_desc="CBT Nuggets wuz here")
 
-with manager.connect(**router, hostkey_=False) as m:
+with manager.connect(**router, hostkey_verify=False) as m:
     response = m.edit_config(netconf_config, target="candidate")
 
 print(response)
