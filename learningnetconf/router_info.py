0a5d2385ffa3de2ae1202f3c8e24abfe30d36c2d
Micah Cerasani <Micah@Cerasani.net>
2023-07-01 08:50:55 +0000

    a second round of cleaning

--- learningnetconf/router_info.py
+++
@@ -1,9 +0,0 @@
-#router = {"host":"sandbox-iosxe-recomm-1.cisco.com",
-#          "port":"830",
-#          "username":"developer",
-#          "password": "lastorangerestoreball8876"}
-
-router = {"host": "ios-xe-mgmt-latest.cisco.com",
-          "port": "10000",
-          "username": "root",
-          "password": "D_Vay!_10&"}
