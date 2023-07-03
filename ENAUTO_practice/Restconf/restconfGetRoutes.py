383c298303c899264f886c09bc40e58837748159
Micah Cerasani <Micah@Cerasani.net>
2023-07-03 00:22:38 +0000

    Getting Data with Restconf

---
+++ ENAUTO_practice/Restconf/restconfGetRoutes.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+router = {
+    "host": "sandbox-iosxe-recomm-1.cisco.com",
+    "port": "443",
+    "user": "developer",
+    "password": "lastorangerestoreball8876"
+}
+
+
+headers = {
+    "Accept":"application/yang-data+json",
+    "Content-Type":"appllication/yang-data+jason"    
+}
+
+# url =f"https://{router['host']}:{router['port']}/restconf/data/ietf-routing:routing"
+
+# response = requests.get(url=url, headers=headers, auth=(router["user"], router["password"]), verify=False).json()
+
+# print(json.dumps(response, indent=2))
+
+
+url =f"https://{router['host']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=GigabitEthernet2"
+
+response = requests.get(url=url, headers=headers, auth=(router["user"], router["password"]), verify=False).json()
+
+print(json.dumps(response, indent=2))
+
